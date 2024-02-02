21a267c309094c4263ef1a3b97fd258bcf40ab7e
supercoolk <157793261+supercoolk@users.noreply.github.com>
2024-02-02 11:02:18 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        getal += -1
+        basic.showNumber(getal)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        basic.showNumber(getal)
+        getal += 1
+    }
+})
+let getal = 0
+getal = 0
